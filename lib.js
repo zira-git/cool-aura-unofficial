bac8729299217adffd7ec3da95df3a125e5f3cbb
emad <Emad Mirza>
2026-02-18 19:59:45 -0500

    Search_vesrion 2

--- lib.js
+++ lib.js
@@ -1,10 +1,14 @@
 function search() {
     let url = document.getElementById("search").value
-    if(url.includes('.') || url.includes('https://') || url.includes('http://')) {
+    if(url.includes('.')) {
         console.log(url)
         console.log(url)
-        window.location.href = url;
-    } else {
+        window.location.href = `https://${url}`
+    } else if(url === "") {
+        document.getElementById('search').setAttribute('placeholder','Search Box Cannot Be Empty')
+    }
+    
+    else {
         window.location.href = `https://google.com/search?q=${url}&safe=active&ssui=on`;
     }
 }
