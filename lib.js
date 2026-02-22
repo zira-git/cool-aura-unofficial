7f0c504990cdac96be47afe312d7591f29b07a47
zira <ziraproton@proton.me>
2026-02-22 18:21:12 -0500

    Update lib.js

--- lib.js
+++ lib.js
@@ -11,33 +11,37 @@ function search() {
     } else if(url === "") {
         document.getElementById('search').setAttribute('placeholder','The search bar cannot be empty.')
     }
-    
+
     else if(localStorage.getItem('searchengine') === 'google') {
         window.location.href = `https://google.com/search?q=${url}&safe=active&ssui=on`;
     }
-        
+
     else if(localStorage.getItem('searchengine') === 'duckduckgo') {
         window.location.href = `https://duckduckgo.com/?q=${url}&ia=web`;
     }
-        
+
     else if(localStorage.getItem('searchengine') === 'startpage') {
         window.location.href = `https://www.startpage.com/sp/search?query=${url}`
     }
-        
+
     else if(localStorage.getItem('searchengine') === 'vyntr') {
         window.location.href = `https://vyntr.com/search?q=${url}`
     }
-        
+
     else if(localStorage.getItem('searchengine') === 'ecosia') {
         window.location.href = `https://www.ecosia.org/search?method=index&q=${url}`
     }
-        
+
     else if(localStorage.getItem('searchengine') === 'brave') {
         window.location.href = `https://search.brave.com/search?q=${url}`
     }
-        
+
     else if(localStorage.getItem('searchengine') === 'yandex') {
         window.location.href = `https://yandex.com/search/?text=${url}`
+    }
+
+    else if(localStorage.getItem('searchengine') === 'bliptext') {
+        window.location.href = `https://bliptext.com/search?q=${url}`
     }
 }
 
